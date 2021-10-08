93285723597653bcd5e6f7db57ed0ce00befc881
Frédéric Langlade-Bellone <fred@parapluie.org>
2021-10-08 12:14:37 +0200

    init: first project version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { ThemeProvider } from "@fluentui/react";
+import { initializeIcons } from "@uifabric/icons";
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Error from "./components/Error";
+import Header from "./components/Header";
+import Loader from "./components/Loader";
+import Section from "./components/Section";
+import useData, { DataContext } from "./hooks/useData";
+import initLocales from "./i18n";
+import Home from "./pages/Home";
+import Patch from "./pages/Patch";
+import myTheme from "./theme/fluenceTheme";
 
-function App() {
+initializeIcons();
+
+initLocales();
+
+const Inner: React.FC = () => {
+  const { load, ...dataAPI } = useData();
+
+  useEffect(() => load(), [load]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DataContext.Provider value={dataAPI}>
+      <Header />
+      {!dataAPI.isLoaded && <Loader full />}
+      {dataAPI.isLoaded && dataAPI.isError && <Error />}
+      {dataAPI.isLoaded && !dataAPI.isError && (
+        <Section>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/search/:searchState" component={Home} />
+            <Route path="/patch/:id" component={Patch} />
+          </Switch>
+        </Section>
+      )}
+    </DataContext.Provider>
   );
-}
+};
+
+const App: React.FC = () => (
+  <ThemeProvider applyTo="body" theme={myTheme}>
+    <Router>
+      <Inner />
+    </Router>
+  </ThemeProvider>
+);
 
 export default App;
